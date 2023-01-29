466d9a2effec1e6a2f87cdd4a26b4ef4633bf0b8
mitchelinoue <mitchel_inoue@msn.com>
2023-01-28 22:11:30 -0300

    final exercise week 2 and start of week 3

--- week_2/substitution/substitution.c
+++ week_2/substitution/substitution.c
@@ -1,51 +1,118 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+
+bool quantity(int n);
+bool alphanumeric(string key, int n);
+bool unique(string key, int n);
 
 int main(int argc, string argv[])
 {
-
-    // get key
     string key;
-    if (argc == 2)
+    int n;
+
+    if (argc != 2)
     {
-        key = argv[1];
-        validate_key(key);
-    }
-    else if()
-
-
-
-
-    else
-    {
-        printf("Usage: %s KEY", argv[0]);
+        printf("Usage: %s KEY\n", argv[0]);
+        return 1;
     }
 
-    // validate key
+    key = argv[1];
+    n = strlen(key);
+    if (!quantity(n))
+    {
+        printf("Key must contain 26 character.\n");
+        return 1;
+    }
 
-    // get plaintext
+    if (!alphanumeric(key, n))
+    {
+        printf("Key must only contain alphabetic characters.\n");
+        return 1;
+    }
 
-    //encipher
+    if (!unique(key, n))
+    {
+        printf("Key must not contain repeated characters.\n");
+        return 1;
+    }
 
-    // print ciphertext
+    string plaintext = get_string("plaintext: ");
+    int m = strlen(plaintext);
+    printf("ciphertext: ");
+
+    for (int i = 0; i < m; i++)
+    {
+        if (plaintext[i] > 96 && plaintext[i] < 123)
+        {
+            int c = plaintext[i] - 97;
+
+            printf("%c", tolower(key[c]));
+        }
+        else if (plaintext[i] > 64 && plaintext[i] < 91)
+        {
+            int c = plaintext[i] - 65;
+
+            printf("%c", toupper(key[c]));
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
+    return 0;
 }
 
-bool validate_key(string key)
+bool quantity(int n)
 {
-    int n = strlen(key);
     if (n != 26)
     {
-        printf("Key must contain 26 chatacter.");
-        retun 0;
+        return false;
     }
     else
     {
-        bool is_char = true;
-        int index = 0;
-        while (is_char == true && index < n)
-        {
-            is_char = isalpha()
-        }
-        return is_char;
+        return true;
     }
+}
+
+bool alphanumeric(string key, int n)
+{
+    bool is_char = true;
+    int index = 0;
+    while (is_char == true && index < n)
+    {
+        if (isalpha(key[index]))
+        {
+            is_char = true;
+            index++;
+        }
+        else
+        {
+            is_char = false;
+        }
+    }
+    return is_char;
+}
+
+bool unique(string key, int n)
+{
+    bool is_unique = true;
+    int index = 0;
+    while (is_unique == true && index < n)
+    {
+        if (strchr(key, key[index]) == strrchr(key, key[index]))
+        {
+            is_unique = true;
+            index++;
+        }
+        else
+        {
+            is_unique = false;
+        }
+    }
+    return is_unique;
 }
